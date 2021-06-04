0938a57929b819049a0414abf2d214fd56d7cfac
Gatsby <liugatsby17@gmail.com>
2021-06-04 14:46:16 +0800

    伪数组：有 "0-(length-1)" 的属性， 并且有 "length" 属性

--- js/06-jQuery对象.js
+++ js/06-jQuery对象.js
@@ -1,6 +1,6 @@
 $(function () {
     /* 1.jQuery对象是一个 伪数组
-       伪数组：有 0-(length-1) 的属性， 并且有 length 属性
+       伪数组：有 "0-(length-1)" 的属性， 并且有 "length" 属性
      */
     var $div = $("div");
     console.dir($div);
